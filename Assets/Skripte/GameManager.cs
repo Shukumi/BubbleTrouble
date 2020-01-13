676d6609b15cbceda191ba7fb8dc8c6c2a7e9752
Bla <zog.gen@hotmail.de>
2020-01-13 07:33:17 +0100

    Main-Logic

--- Assets/Skripte/GameManager.cs
+++ Assets/Skripte/GameManager.cs
@@ -7,34 +7,67 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField]
     private Rigidbody2D BallRigid;
-    public float kraft = 20.0f;
+    public float kraft = 350.0f;
+    //private float kraft2 = 350.0f;
     private Vector3 velocity;
-   //  Rigidbody2D BallRigid;
+    Vector2 direction;
+
+
+    //  Rigidbody2D BallRigid;
     void Start()
     {
-      //  BallRigid = Ball.GetComponent<Rigidbody2D>();
-        BallRigid.AddForce(transform.right * kraft);
+        //  BallRigid = Ball.GetComponent<Rigidbody2D>();
+      
+        direction = new Vector2(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f));
+        choseDirection(direction);
+      //  BallRigid.AddForce(transform.right * kraft);
+       // BallRigid.AddForce(transform.up * kraft);
     }
 
     // Update is called once per frame
     
-    void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        ReflectProjectile(BallRigid, collision.contacts[0].normal);
-        print("LUL");
+        //ReflectProjectile(BallRigid, collision.contacts[0].normal);
+        if (collision.gameObject.tag == "Rope")
+        {
+            print("PUNKT!");
+        }
+        BallRigid.velocity = Vector2.zero;
+        Vector2 CollisionNormal = collision.contacts[0].normal;
+
+        ReflectProjectile(BallRigid, collision.GetContact(0).normal);
+        //print("LUL");
     }
 
+    private void choseDirection(Vector2 inDirection)
+    {
+        direction = inDirection;
+
+        //Normalize directional vector
+        direction.Normalize();
+
+        if (kraft >= 0)
+            kraft = 350.0f;
+
+        //add force in the direction the ball bounces or starts
+        BallRigid.AddForce(direction * kraft);
+    }
 
     private void ReflectProjectile(Rigidbody2D rb, Vector3 reflectVector)
     {
-        velocity = Vector3.Reflect(velocity, reflectVector);
+        direction = Vector3.Reflect(direction, reflectVector);
+        choseDirection(direction);
+        /*
         print("LUXD");
-        rb.velocity = velocity;
+        velocity.Normalize();
+        BallRigid.AddForce(velocity * kraft2);
+        */
     }
 
     
     void Update()
     {
-        
+
     }
 }
---
+++ Assets/Skripte/PlayerManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float canJump = 0f;
+    [SerializeField]
+    private Rigidbody2D RigidPlayer;
+    float speed = 10.0f;
+    float RopeDifference = 1.03f;
+    float RopeLength = 0.732f;
+    public Transform Rope;
+    private Transform CurrentRope;
+    private List<GameObject> currentRopes;
+    void Start()
+    {
+        RigidPlayer.freezeRotation = true;
+        currentRopes = new List<GameObject>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+        RigidPlayer.AddForce(movement * speed);
+        RigidPlayer.velocity = new Vector2(0, 0);
+        */
+        this.transform.Translate(Input.GetAxis("Horizontal")/10, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > canJump)
+        {
+
+            RigidPlayer.AddForce(new Vector3(0, 6, 0), ForceMode2D.Impulse);
+            canJump = Time.time + 1.95f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            print("InsertPressed");
+            Vector3 currentPosition = this.transform.position;
+            currentPosition.y += RopeDifference;
+            CurrentRope = Instantiate(Rope, currentPosition, Quaternion.identity);
+            currentRopes.Add(CurrentRope.gameObject);
+            StartCoroutine(ExpandRope());
+        }
+    }
+
+    private IEnumerator ExpandRope()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            yield return new WaitForSeconds(0.4f);
+            Vector3 currentPosition = CurrentRope.position;
+            currentPosition.y += RopeLength;
+            CurrentRope = Instantiate(Rope, currentPosition, Quaternion.identity);
+            currentRopes.Add(CurrentRope.gameObject);
+
+            /*
+            CurrentRope.localScale += new Vector3(0, CurrentRope.localScale.y, 0);
+            Vector3 currentPosition = CurrentRope.position;
+            currentPosition.y -= CurrentRope.position.y;
+            CurrentRope.position = currentPosition;
+            print("lul");
+            */
+
+        }
+        yield return new WaitForSeconds(0.6f);
+
+        foreach (GameObject item in currentRopes)
+        {
+            Destroy(item);
+        }
+
+        
+    }
+}
+
+// rope y 0,85
